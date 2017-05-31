3b3db150c3f3e18f5ee72d5bd131e9544e9eafa9
Anton Sivov <anton.sivov@inbox.ru>
2017-05-31 20:32:43 +0300

    Added Auth Repository class

---
+++ src/Lykke.Pkg.AzureRepositories/MerchantRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lykke.Core;
+using Lykke.Core.Azure;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Lykke.AzureRepositories
+{
+
+    public class MerchantEntity : TableEntity, IMerchantEntity
+    {
+        public static string GeneratePartitionKey()
+        {
+            return "M";
+        }
+
+        public string MerchantId
+        {
+            get => RowKey;
+            set => RowKey = value;
+        }
+
+        public string MerchantName { get; set; }
+        public string PublicKey { get; set; }
+        public string ApyKey { get; set; }
+    }
+
+    public class MerchantRepository : IMerchantRepository
+    {
+        private readonly INoSQLTableStorage<MerchantEntity> _tableStorage;
+
+        public MerchantRepository(INoSQLTableStorage<MerchantEntity> tableStorage)
+        {
+            _tableStorage = tableStorage;
+
+        }
+
+        public async Task<IMerchantEntity> GetAsync(string merchantId)
+        {
+            var pk = TokenEntity.GeneratePartitionKey();
+             return await _tableStorage.GetDataAsync(pk, merchantId);
+        }
+
+        public async Task<IEnumerable<IMerchantEntity>> GetAllAsync()
+        {
+            var pk = TokenEntity.GeneratePartitionKey();
+
+            return await _tableStorage.GetDataAsync(pk);
+        }
+    }
+}
---
+++ src/Lykke.Pkg.Core/IMerchantRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lykke.Core
+{
+    public interface IMerchantEntity : IEntity
+    {
+        string MerchantId { get; set; }
+        string MerchantName { get; set; }
+        string PublicKey { get; set; }
+        string ApyKey { get; set; }
+    }
+
+    public interface IMerchantRepository
+    {
+        Task<IMerchantEntity> GetAsync(string merchantId);
+
+        Task<IEnumerable<IMerchantEntity>> GetAllAsync();
+    }
+}
