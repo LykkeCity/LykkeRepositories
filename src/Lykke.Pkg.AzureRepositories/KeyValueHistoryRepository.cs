b00f16ef4ce3c29b87c2f878bc19db106b752992
Anton Sivov <anton.sivov@inbox.ru>
2017-06-08 17:27:00 +0300

    [LWDEV-2060] - Done

---
+++ src/Lykke.Pkg.AzureRepositories/KeyValueHistoryRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Lykke.AzureRepositories.Extentions;
+using Lykke.Core;
+using Lykke.Core.Azure;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Lykke.AzureRepositories
+{
+    public class KeyValueHistory : TableEntity, IKeyValueHistory
+    {
+        public static string GeneratePartitionKey()
+        {
+            return "KVH";
+        }
+
+
+        public string KeyValues { get; set; }
+        public string UserName { get; set; }
+        public string UserIpAddress { get; set; }
+    }
+    public class KeyValueHistoryRepository : IKeyValueHistoryRepository
+    {
+        private readonly INoSQLTableStorage<KeyValueHistory> _tableStorage;
+
+        public KeyValueHistoryRepository(INoSQLTableStorage<KeyValueHistory> tableStorage)
+        {
+            _tableStorage = tableStorage;
+
+        }
+
+        public async Task SaveTokenHistoryAsync(string keyValues, string userName, string userIpAddress)
+        {
+            var th = new KeyValueHistory
+            {
+                PartitionKey = KeyValueHistory.GeneratePartitionKey(),
+                RowKey = DateTime.UtcNow.StorageString(),
+                UserName = userName,
+                KeyValues = keyValues,
+                UserIpAddress = userIpAddress
+            };
+
+            await _tableStorage.InsertOrMergeAsync(th);
+        }
+
+        
+    }
+}
---
+++ src/Lykke.Pkg.Core/IKeyValueHistoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lykke.Core
+{
+    public interface IKeyValueHistory : IEntity
+    {
+        string KeyValues { get; set; }
+        string UserName { get; set; }
+        string UserIpAddress { get; set; }
+    }
+
+    public interface IKeyValueHistoryRepository
+    {
+        Task SaveTokenHistoryAsync(string keyValues, string userName, string userIpAddress);
+    }
+}
