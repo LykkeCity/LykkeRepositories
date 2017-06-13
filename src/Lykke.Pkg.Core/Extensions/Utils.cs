31c5ffaa99d7bd795804dad4a9ef220c89bfdccb
Anton Sivov <anton.sivov@inbox.ru>
2017-06-13 17:33:50 +0300

    Added Encript/Decript extentins

--- src/Lykke.Pkg.Core/Extensions/Utils.cs
+++ src/Lykke.Pkg.Core/Extensions/Utils.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace Lykke.Core
 {
@@ -73,5 +76,59 @@ namespace Lykke.Core
 				return defaultValue;
 			}
 		}
-	}
+
+	    public static byte[] Encrypt<T>(this T src, string key)
+	    {
+	        byte[] result;
+	        using (var aes = Aes.Create())
+	        using (var md5 = MD5.Create())
+	        using (var sha256 = SHA256.Create())
+	        {
+	            aes.Key = sha256.ComputeHash(Encoding.UTF8.GetBytes(key));
+	            aes.IV = md5.ComputeHash(Encoding.UTF8.GetBytes(key));
+
+	            using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+	            using (var resultStream = new MemoryStream())
+	            {
+	                using (var aesStream = new CryptoStream(resultStream, encryptor, CryptoStreamMode.Write))
+	                using (var plainStream = new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(src))))
+	                {
+	                    plainStream.CopyTo(aesStream);
+	                }
+
+	                result = resultStream.ToArray();
+	            }
+	        }
+
+           
+	            return result;
+	    }
+
+	    public static T Decrypt<T>(this byte[] message, string key)
+	    {
+	        byte[] result;
+	        using (var aes = Aes.Create())
+	        using (var md5 = MD5.Create())
+	        using (var sha256 = SHA256.Create())
+	        {
+	            aes.Key = sha256.ComputeHash(Encoding.UTF8.GetBytes(key));
+	            aes.IV = md5.ComputeHash(Encoding.UTF8.GetBytes(key));
+
+	            using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+	            using (var resultStream = new MemoryStream())
+	            {
+	                using (var aesStream = new CryptoStream(resultStream, decryptor, CryptoStreamMode.Write))
+	                using (var plainStream = new MemoryStream(message))
+	                {
+	                    plainStream.CopyTo(aesStream);
+	                }
+
+	                result = resultStream.ToArray();
+	            }
+	        }
+
+            
+	        return JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(result));
+	    }
+    }
 }
---
+++ tests/Lykke.AzureRepositories.Test/ExtentionTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Lykke.Core;
+using Xunit;
+
+namespace Lykke.AzureRepositories.Test
+{
+    public class ExtentionTests
+    {
+        private readonly string _key = "test";
+        private readonly string _encriptedString = "hfJd1cwIdDe5w8T2pRV1yA==";
+        private readonly List<int> _testData = new List<int> {
+            5,6,7,8,10
+        };
+
+        [Fact]
+        public void EncriptionTest()
+        {
+            var s = _testData.Encrypt(_key);
+            Assert.Equal(Convert.ToBase64String(s), _encriptedString);
+        }
+
+
+        [Fact]
+        public void DecriptionTest()
+        {
+            var s = Convert.FromBase64String(_encriptedString).Decrypt<List<int>>(_key);
+            Assert.Equal(5, s.Count);
+        }
+    }
+}
