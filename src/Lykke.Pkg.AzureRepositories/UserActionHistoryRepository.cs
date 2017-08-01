c7d931a0c4710e1aa8aced20245c7e0d40c2fe36
Anton Sivov <anton.sivov@inbox.ru>
2017-08-01 14:57:15 +0300

    Added User Active Repository

---
+++ src/Lykke.Pkg.AzureRepositories/UserActionHistoryRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Lykke.AzureRepositories.Extentions;
+using Lykke.Core;
+using Lykke.Core.Azure;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Lykke.AzureRepositories
+{
+    public class UserActionHistoryEntity : TableEntity, IUserActionHistoryEntity
+    {
+        public string UserEmail { get; set; }
+        public DateTime ActionDate { get; set; }
+        public string IpAddress { get; set; }
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+        public string Params { get; set; }
+
+        public static string GeneratePartitionKey()
+        {
+            return "UAH";
+        }
+
+        public string GetRawKey()
+        {
+            return ActionDate.StorageString();
+        }
+
+        public static UserActionHistoryEntity Create(IUserActionHistoryEntity entity)
+        {
+            return new UserActionHistoryEntity
+            {
+                UserEmail = entity.UserEmail,
+                ActionDate = entity.ActionDate,
+                IpAddress = entity.IpAddress,
+                ControllerName = entity.ControllerName,
+                ActionName = entity.ActionName,
+                Params = entity.Params,
+                PartitionKey = GeneratePartitionKey(),
+                RowKey = entity.ActionDate.StorageString()
+            };
+        }
+    }
+    public class UserActionHistoryRepository : IUserActionHistoryRepository
+    {
+        private readonly INoSQLTableStorage<UserActionHistoryEntity> _tableStorage;
+        private readonly IBlobStorage _blobStorage;
+        private readonly string _container;
+
+        public UserActionHistoryRepository(INoSQLTableStorage<UserActionHistoryEntity> tableStorage, IBlobStorage blobStorage, string container)
+        {
+            _tableStorage = tableStorage;
+            _blobStorage = blobStorage;
+            _container = container;
+        }
+
+        public async Task SaveUserActionHistoryAsync(IUserActionHistoryEntity userActionHistory)
+        {
+            var entity = UserActionHistoryEntity.Create(userActionHistory);
+            if (!string.IsNullOrEmpty(entity.Params))
+            {
+                var parms = entity.Params;
+                entity.Params = Guid.NewGuid().ToString();
+
+                var data = Encoding.UTF8.GetBytes(parms);
+                await _blobStorage.SaveBlobAsync(_container, entity.Params, data);
+               
+            }
+           
+
+            await _tableStorage.InsertOrMergeAsync(entity);
+        }
+    }
+}
---
+++ src/Lykke.Pkg.Core/IUserActionHistoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Lykke.Core
+{
+    public interface IUserActionHistoryEntity
+    {
+        string UserEmail { get; set; }
+        DateTime ActionDate { get; set; }
+        string IpAddress { get; set; }
+        string ControllerName { get; set; }
+        string ActionName { get; set; }
+        string Params { get; set; }
+    }
+
+    public interface IUserActionHistoryRepository
+    {
+        Task SaveUserActionHistoryAsync(IUserActionHistoryEntity userActionHistory);
+    }
+}
