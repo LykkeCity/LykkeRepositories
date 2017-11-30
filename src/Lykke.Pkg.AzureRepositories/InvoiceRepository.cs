5223dbcf355dacb303bd4dc30759d6a0050f92c9
Anton Sivov <anton.sivov@inbox.ru>
2017-11-30 14:01:24 +0300

    Added repo for Invoices

---
+++ src/Lykke.Pkg.AzureRepositories/InvoiceRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lykke.Core;
+using Lykke.Core.Azure;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Lykke.AzureRepositories
+{
+    public class InvoiceEntity : TableEntity, IInvoiceEntity
+    {
+        public InvoiceEntity()
+        {
+            PartitionKey = GeneratePartitionKey();
+        }
+
+        public string InvoiceId
+        {
+            get => RowKey;
+            set => RowKey = value;
+        }
+
+        public string InvoiceNumber { get; set; }
+        public double Amount { get; set; }
+        public string Currency { get; set; }
+
+        public static string GeneratePartitionKey()
+        {
+            return "I";
+        }
+    }
+
+    public class InvoiceRepository: IInvoiceRepository
+    {
+        private readonly INoSQLTableStorage<InvoiceEntity> _tableStorage;
+
+        public InvoiceRepository(INoSQLTableStorage<InvoiceEntity> tableStorage)
+        {
+            _tableStorage = tableStorage;
+        }
+
+        public async Task<bool> SaveInvoice(IInvoiceEntity invoice)
+        {
+            try
+            {
+                var store = new InvoiceEntity
+                {
+                    InvoiceNumber = invoice.InvoiceNumber,
+                    Amount = invoice.Amount,
+                    Currency = invoice.Currency
+                };
+                await _tableStorage.InsertOrMergeAsync(store);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            
+        }
+
+        public async Task<List<IInvoiceEntity>> GetInvoices()
+        {
+            var pk = InvoiceEntity.GeneratePartitionKey();
+            return (await _tableStorage.GetDataAsync(pk)).Cast<IInvoiceEntity>().ToList();
+        }
+
+        public async Task<IInvoiceEntity> GetInvoice(string invoiceId)
+        {
+            var pk = InvoiceEntity.GeneratePartitionKey();
+            return await _tableStorage.GetDataAsync(pk, invoiceId);
+        }
+    }
+}
---
+++ src/Lykke.Pkg.Core/IInvoiceRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lykke.Core
+{
+
+    public interface IInvoiceEntity
+    {
+        string InvoiceId { get; set; }
+        string InvoiceNumber { get; set; }
+        double Amount { get; set; }
+        string Currency { get; set; }
+    }
+
+    public interface IInvoiceRepository
+    {
+        Task<bool> SaveInvoice(IInvoiceEntity invoice);
+        Task<List<IInvoiceEntity>> GetInvoices();
+        Task<IInvoiceEntity> GetInvoice(string invoiceId);
+    }
+}
